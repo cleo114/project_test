3bc0c99657f32906c5d83ee13271618a2cae1298
Cleo Sabath <cleosabath@cleos-mbp.lan>
2022-05-22 21:01:19 +0800

    updated testing files

--- tests/test_models.py
+++ tests/test_models.py
@@ -26,20 +26,69 @@ class Models (unittest.TestCase):
         s.set_password('hello')
         self.assertFalse(s.check_password('goodbye'))
         self.assertTrue(s.check_password('hello'))
-    
-    # def test_is_committed(self):
-    #     s = Score.query.get('00000000')
-    #     self.assertFalse(s.is_committed())
-    #     # usr=User.query.get("00000000")
-    #     # sc = Score(user_id=usr.id,points=15)
-    #     # db.session.add(sc)
-    #     # db.session.flush()
-    #     # s.user_id = sc.id
-    #     # db.session.commit()
-    #     # self.assertTrue(s.is_committed())
-
-
-
 
 if __name__=='__main__':
     unittest.main()
+
+
+
+class AppFunction(unittest.TestCase):
+    def setUp(self):
+        self.app = app
+        self.app.config['WTF_CSRF_ENABLED'] = False  
+        self.appctx = self.app.app_context()
+        self.appctx.push()
+        db.create_all()
+        self.client = self.app.test_client()
+
+    def tearDown(self):
+        db.drop_all()
+        self.appctx.pop()
+        self.app = None
+        self.appctx = None
+        self.client = None
+
+    def test_app(self):
+        assert self.app is not None
+        assert app == self.app
+
+    def test_home_page(self):
+        response = self.client.get('/', follow_redirects=True)
+        assert response.status_code == 200
+        assert response.request.path == '/login'
+
+
+    def test_registration(self):
+        response = self.client.get('/register')
+        assert response.status_code == 200
+        html = response.get_data(as_text=True)
+
+        assert 'name="username"' in html
+        assert 'name="email"' in html
+        assert 'name="password"' in html
+        assert 'name="password2"' in html
+        assert 'name="submit"' in html
+
+
+    def test_register_user(self):
+        response = self.client.post('/register', data={
+            'username': 'gemma',
+            'email': 'gemma@random.com',
+            'password': 'xxx',
+            'password2': 'xxx',
+        }, follow_redirects=True)
+        assert response.status_code == 200
+        assert response.request.path == '/login' 
+
+
+    def test_mismatched_passwords(self):
+        response = self.client.post('/register', data={
+            'username': 'gemma',
+            'email': 'gemma@random.com',
+            'password': 'xxx',
+            'password2': 'yyy',
+        })
+        assert response.status_code == 200
+        html = response.get_data(as_text=True)
+        assert 'Field must be equal to password.' in html
+
